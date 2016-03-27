b612d417180d3c77bdc961161df5f8fe50c68497
András Péteri <apeteri@b2international.com>
2016-03-27 13:44:37 +0200

    WRP-2302 Add hashCode and equals implementation to relationship...
    ...type and target elements

--- snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/domain/browser/SnomedBrowserRelationshipTarget.java
+++ snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/domain/browser/SnomedBrowserRelationshipTarget.java
@@ -49,6 +49,31 @@ public class SnomedBrowserRelationshipTarget extends SnomedBrowserComponent impl
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((conceptId == null) ? 0 : conceptId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SnomedBrowserRelationshipTarget other = (SnomedBrowserRelationshipTarget) obj;
+		if (conceptId == null) {
+			if (other.conceptId != null)
+				return false;
+		} else if (!conceptId.equals(other.conceptId))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		final StringBuilder builder = new StringBuilder();
 		builder.append("SnomedBrowserRelationshipTarget [conceptId=");
