a3f732020fa08fbce34d877ef304df784f2420bb
Kai Kewley <kaikewley@gmail.com>
2016-05-23 23:37:28 +0100

    WRP-2747 API support forced deletion of released concepts.

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/components/SnomedReleasedConceptApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/components/SnomedReleasedConceptApiTest.java
@@ -1,36 +1,53 @@
 package com.b2international.snowowl.snomed.api.rest.components;
 
+import static com.b2international.snowowl.datastore.BranchPathUtils.createMainPath;
 import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.givenBranchWithPath;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanBeDeleted;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanNotBeDeleted;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanBeUpdated;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentHasProperty;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertConceptExists;
 
 import java.util.Map;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import com.b2international.snowowl.snomed.api.rest.AbstractSnomedApiTest;
 import com.b2international.snowowl.snomed.api.rest.SnomedComponentType;
 import com.b2international.snowowl.snomed.core.domain.DefinitionStatus;
 import com.google.common.collect.ImmutableMap;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SnomedReleasedConceptApiTest extends AbstractSnomedApiTest {
+
+	private static final String CONCEPT_ID = "63961392103";
 
 	@Test
 	public void automaticRestorationOfEffectiveTime() {
 		givenBranchWithPath(testBranchPath);
-		final String conceptId = "63961392103";
-		assertConceptExists(testBranchPath, conceptId);
+		assertConceptExists(testBranchPath, CONCEPT_ID);
 		
-		assertComponentHasProperty(testBranchPath, SnomedComponentType.CONCEPT, conceptId, "effectiveTime", "20150204");
+		assertComponentHasProperty(testBranchPath, SnomedComponentType.CONCEPT, CONCEPT_ID, "effectiveTime", "20150204");
 		
-		updateDefinitionStatus(conceptId, DefinitionStatus.FULLY_DEFINED);
+		updateDefinitionStatus(CONCEPT_ID, DefinitionStatus.FULLY_DEFINED);
 
-		assertComponentHasProperty(testBranchPath, SnomedComponentType.CONCEPT, conceptId, "effectiveTime", null);
+		assertComponentHasProperty(testBranchPath, SnomedComponentType.CONCEPT, CONCEPT_ID, "effectiveTime", null);
 
-		updateDefinitionStatus(conceptId, DefinitionStatus.PRIMITIVE);
+		updateDefinitionStatus(CONCEPT_ID, DefinitionStatus.PRIMITIVE);
 
-		assertComponentHasProperty(testBranchPath, SnomedComponentType.CONCEPT, conceptId, "effectiveTime", "20150204");
+		assertComponentHasProperty(testBranchPath, SnomedComponentType.CONCEPT, CONCEPT_ID, "effectiveTime", "20150204");
+	}
+
+	@Test
+	public void deleteFails() {
+		assertComponentCanNotBeDeleted(createMainPath(), SnomedComponentType.CONCEPT, CONCEPT_ID, false);
+	}
+
+	@Test
+	public void forceDelete() {
+		assertComponentCanBeDeleted(createMainPath(), SnomedComponentType.CONCEPT, CONCEPT_ID, true);
 	}
 
 	private void updateDefinitionStatus(final String conceptId,
--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/SnomedEditingContext.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/SnomedEditingContext.java
@@ -83,6 +83,7 @@ import com.b2international.snowowl.core.api.SnowowlServiceException;
 import com.b2international.snowowl.core.api.browser.IClientTerminologyBrowser;
 import com.b2international.snowowl.core.api.index.IIndexEntry;
 import com.b2international.snowowl.core.exceptions.ComponentNotFoundException;
+import com.b2international.snowowl.core.exceptions.ConflictException;
 import com.b2international.snowowl.core.terminology.ComponentCategory;
 import com.b2international.snowowl.datastore.BranchPathUtils;
 import com.b2international.snowowl.datastore.CDOEditingContext;
@@ -1219,19 +1220,24 @@ public class SnomedEditingContext extends BaseSnomedEditingContext {
 	
 	@Override
 	public void delete(EObject object) {
-		if (object instanceof Concept) {
-			delete((Concept) object);
-		} else if (object instanceof Description) {
-			delete((Description) object);
-		} else if (object instanceof Relationship) {
-			delete((Relationship) object);
-		}
-		super.delete(object);
+		delete(object, false);
 	}
 	
-	private void delete(Concept concept) {
+	@Override
+	public void delete(EObject object, boolean force) throws ConflictException {
+		if (object instanceof Concept) {
+			delete((Concept) object, force);
+		} else if (object instanceof Description) {
+			delete((Description) object, force);
+		} else if (object instanceof Relationship) {
+			delete((Relationship) object, force);
+		}
+		super.delete(object, force);
+	}
+	
+	private void delete(Concept concept, boolean force) {
 		
-		SnomedDeletionPlan deletionPlan = canDelete(concept, null);
+		SnomedDeletionPlan deletionPlan = canDelete(concept, null, force);
 		if(deletionPlan.isRejected()) {
 			throw new IllegalArgumentException(deletionPlan.getRejectionReasons().toString());
 		}
@@ -1239,21 +1245,21 @@ public class SnomedEditingContext extends BaseSnomedEditingContext {
 		delete(deletionPlan);
 	}
 	
-	public SnomedDeletionPlan canDelete(Concept concept, SnomedDeletionPlan deletionPlan) {
+	public SnomedDeletionPlan canDelete(Concept concept, SnomedDeletionPlan deletionPlan, boolean force) {
 		
 		if(deletionPlan == null) {
 			deletionPlan = new SnomedDeletionPlan();
 		}
 		
 		// unreleased -> effective time must be in the future
-		if (concept.isReleased()) {
+		if (concept.isReleased() && !force) {
 			deletionPlan.addRejectionReason(String.format(COMPONENT_IS_RELEASED_MESSAGE, "concept", toString(concept)));
 			return deletionPlan;
 		}
 		
 		for (Relationship relationship : getInboundRelationships(concept.getId())) {
 			if (IS_A.equals(relationship.getType().getId())) {
-				deletionPlan = canDelete(relationship, deletionPlan);
+				deletionPlan = canDelete(relationship, deletionPlan, force);
 				if (deletionPlan.isRejected()) {
 					deletionPlan.addRejectionReason(String.format(UNABLE_TO_DELETE_CONCEPT_MESSAGE, toString(concept)));
 					return deletionPlan;
@@ -1263,7 +1269,7 @@ public class SnomedEditingContext extends BaseSnomedEditingContext {
 		
 		// check descriptions. If the deletion is not legit, we cannot reach this point, hence a check would be meaningless
 		for (Description description : concept.getDescriptions()) {
-			deletionPlan = canDelete(description, deletionPlan);
+			deletionPlan = canDelete(description, deletionPlan, force);
 		}
 		
 		// ===================== check refsets ========================
@@ -1324,8 +1330,8 @@ public class SnomedEditingContext extends BaseSnomedEditingContext {
 		return new SnomedDescriptionReducedQueryAdapter(conceptId, SnomedDescriptionReducedQueryAdapter.SEARCH_DESCRIPTION_CONCEPT_ID);
 	}
 
-	private void delete(Relationship relationship) {
-		SnomedDeletionPlan deletionPlan = canDelete(relationship, null);
+	private void delete(Relationship relationship, boolean force) {
+		SnomedDeletionPlan deletionPlan = canDelete(relationship, null, force);
 		if(deletionPlan.isRejected()) {
 			throw new IllegalArgumentException(deletionPlan.getRejectionReasons().toString());
 		}
@@ -1345,14 +1351,14 @@ public class SnomedEditingContext extends BaseSnomedEditingContext {
 		}
 	}
 	
-	public SnomedDeletionPlan canDelete(Relationship relationship, SnomedDeletionPlan deletionPlan) {
+	public SnomedDeletionPlan canDelete(Relationship relationship, SnomedDeletionPlan deletionPlan, boolean force) {
 		
 		if(deletionPlan == null) {
 			deletionPlan = new SnomedDeletionPlan();
 		}
 		
 		// unreleased -> effective time must be in the future
-		if (relationship.isReleased()) {
+		if (relationship.isReleased() && !force) {
 			deletionPlan.addRejectionReason(String.format(COMPONENT_IS_RELEASED_MESSAGE, "relationship", toString(relationship)));
 			return deletionPlan;
 		}
@@ -1368,22 +1374,22 @@ public class SnomedEditingContext extends BaseSnomedEditingContext {
 		return deletionPlan;
 	}
 	
-	private void delete(Description description) {
-		SnomedDeletionPlan deletionPlan = canDelete(description, null);
+	private void delete(Description description, boolean force) {
+		SnomedDeletionPlan deletionPlan = canDelete(description, null, force);
 		if(deletionPlan.isRejected()) {
 			throw new IllegalArgumentException(deletionPlan.getRejectionReasons().toString());
 		}
 		delete(deletionPlan);
 	}
 	
-	public SnomedDeletionPlan canDelete(Description description, SnomedDeletionPlan deletionPlan) {
+	public SnomedDeletionPlan canDelete(Description description, SnomedDeletionPlan deletionPlan, boolean force) {
 		
 		if (deletionPlan == null) {
 			// this was called from outside. Deletion eligibility must be checked
 			deletionPlan = new SnomedDeletionPlan();
 			
 			// unreleased -> effective time must be in the future
-			if (description.isReleased()) {
+			if (description.isReleased() && !force) {
 				deletionPlan.addRejectionReason(String.format(COMPONENT_IS_RELEASED_MESSAGE, "description", toString(description)));
 				return deletionPlan;
 			}
