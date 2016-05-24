1af9ee9b3e196c05150e47e8426ed594ee5d47e8
Kai Kewley <kaikewley@gmail.com>
2016-05-24 14:49:58 +0100

    WRP-2747 Description deletion unit test.

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/components/SnomedReleasedConceptApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/components/SnomedReleasedConceptApiTest.java
@@ -3,10 +3,13 @@ package com.b2international.snowowl.snomed.api.rest.components;
 import static com.b2international.snowowl.datastore.BranchPathUtils.createMainPath;
 import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.givenBranchWithPath;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanBeDeleted;
-import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanNotBeDeleted;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanBeUpdated;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCanNotBeDeleted;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentHasProperty;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertConceptExists;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertConceptNotExists;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertDescriptionExists;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertDescriptionNotExists;
 
 import java.util.Map;
 
@@ -14,6 +17,7 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.b2international.snowowl.core.api.IBranchPath;
 import com.b2international.snowowl.snomed.api.rest.AbstractSnomedApiTest;
 import com.b2international.snowowl.snomed.api.rest.SnomedComponentType;
 import com.b2international.snowowl.snomed.core.domain.DefinitionStatus;
@@ -23,6 +27,8 @@ import com.google.common.collect.ImmutableMap;
 public class SnomedReleasedConceptApiTest extends AbstractSnomedApiTest {
 
 	private static final String CONCEPT_ID = "63961392103";
+	private static final String DESCRIPTION_ID = "11320138110";
+	private final IBranchPath mainPath = createMainPath();
 
 	@Test
 	public void automaticRestorationOfEffectiveTime() {
@@ -41,13 +47,31 @@ public class SnomedReleasedConceptApiTest extends AbstractSnomedApiTest {
 	}
 
 	@Test
-	public void deleteFails() {
-		assertComponentCanNotBeDeleted(createMainPath(), SnomedComponentType.CONCEPT, CONCEPT_ID, false);
+	public void deleteFailsConcept() {
+		assertConceptExists(mainPath, CONCEPT_ID);
+		assertComponentCanNotBeDeleted(mainPath, SnomedComponentType.CONCEPT, CONCEPT_ID, false);
+		assertConceptExists(mainPath, CONCEPT_ID);
 	}
 
 	@Test
-	public void forceDelete() {
-		assertComponentCanBeDeleted(createMainPath(), SnomedComponentType.CONCEPT, CONCEPT_ID, true);
+	public void deleteFailsDescription() {
+		assertDescriptionExists(mainPath, DESCRIPTION_ID);
+		assertComponentCanNotBeDeleted(mainPath, SnomedComponentType.DESCRIPTION, DESCRIPTION_ID, false);
+		assertDescriptionExists(mainPath, DESCRIPTION_ID);
+	}
+
+	@Test
+	public void forceDeleteConcept() {
+		assertConceptExists(mainPath, CONCEPT_ID);
+		assertComponentCanBeDeleted(mainPath, SnomedComponentType.CONCEPT, CONCEPT_ID, true);
+		assertConceptNotExists(mainPath, CONCEPT_ID);
+	}
+
+	@Test
+	public void forceDeleteDescription() {
+		assertDescriptionExists(mainPath, DESCRIPTION_ID);
+		assertComponentCanBeDeleted(mainPath, SnomedComponentType.DESCRIPTION, DESCRIPTION_ID, true);
+		assertDescriptionNotExists(mainPath, DESCRIPTION_ID);
 	}
 
 	private void updateDefinitionStatus(final String conceptId,
