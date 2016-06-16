3ffc4495a204975ae19a2acde74bb7e3fc88e381
Kai Kewley <kaikewley@gmail.com>
2016-06-16 13:34:46 +0100

    Update release build script including option for branch name prefix for managed service.

--- build-scripts/release.sh
+++ build-scripts/release.sh
@@ -4,60 +4,63 @@
 #  Changes in develop will be merged to master. The master and develop versions will be bumped. Branches will be pushed back to git.
 ##
 
+branchPrefix=$1
+echo "branch prefix '${branchPrefix}'"
+
 set -e
-echo "Checking out develop."
-git checkout develop
+echo "Checking out ${branchPrefix}develop"
+git checkout "${branchPrefix}develop"
 echo "Pulling commits."
 git pull
-echo "Checking out master."
-git checkout master
+echo "Checking out ${branchPrefix}master."
+git checkout "${branchPrefix}master"
 echo "Pulling commits."
 git pull
-echo "Merging develop into master."
-git merge develop
+echo "Merging ${branchPrefix}develop into ${branchPrefix}master."
+git merge "${branchPrefix}develop"
 
 snapVersion=`grep -m1 '<version>' pom.xml | sed 's/.*>\(.[^<]*\).*/\1/'`
 newRelease=`echo $snapVersion | sed 's/-SNAPSHOT//'`
 newMinor=`echo $((\`echo $newRelease | sed 's/[0-9]*\.[0-9]*\.\([0-9]*\)/\1/'\` + 1))`
-newSnap=`echo -n $newRelease | sed 's/\([0-9]*\)$//' | tr -d '\n';echo "${newMinor}-SNAPSHOT"`
+newSnap=`echo $newRelease | sed 's/\([0-9]*\)$//' | tr -d '\n';echo "${newMinor}-SNAPSHOT"`
 
-echo "Updating master version. New release version is '${newRelease}'"
+echo "Updating ${branchPrefix}master version. New release version is '${newRelease}'"
 mvn -Pdependencies -Dtycho.mode=maven \
  org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="${newRelease}"
 
-echo "Master root pom changes."
+echo "${branchPrefix}master root pom changes."
 git diff pom.xml
 
 #read -p "Commit master changes? " -n 1 -r; echo; if [[ ! $REPLY =~ ^[Yy]$ ]]; then echo "Exiting"; exit 1; fi
 
-echo "Commiting master changes."
-git commit -a -m "IHTSDO Release ${newRelease}"
+echo "Commiting ${branchPrefix}master changes."
+git commit -a -m "IHTSDO ${branchPrefix}Release ${newRelease}"
 
 git log -n5
 #read -p "Push master changes? " -n 1 -r; echo; if [[ ! $REPLY =~ ^[Yy]$ ]]; then echo "Exiting"; exit 1; fi
-echo "Pushing master."
-git push
+echo "Pushing ${branchPrefix}master."
+# git push
 
 
-echo "Checking out develop."
-git checkout develop
+echo "Checking out ${branchPrefix}develop."
+git checkout "${branchPrefix}develop"
 
-echo "Merging master into develop."
-git merge master
+echo "Merging ${branchPrefix}master into ${branchPrefix}develop."
+git merge "${branchPrefix}master"
 
-echo "Updating develop version. New Snapshot version is '${newSnap}'"
+echo "Updating ${branchPrefix}develop version. New Snapshot version is '${newSnap}'"
 mvn -Pdependencies -Dtycho.mode=maven \
  org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="${newSnap}"
 
-echo "Develop root pom changes."
+echo "${branchPrefix}develop root pom changes."
 git diff pom.xml
 
 # read -p "Commit develop changes? " -n 1 -r; echo; if [[ ! $REPLY =~ ^[Yy]$ ]]; then echo "Exiting"; exit 1; fi
 
-echo "Commiting develop changes."
-git commit -a -m "IHTSDO dev version ${newSnap}"
+echo "Commiting ${branchPrefix}develop changes."
+git commit -a -m "IHTSDO ${branchPrefix}dev version ${newSnap}"
 
 git log -n5
 # read -p "Push develop changes? " -n 1 -r; echo; if [[ ! $REPLY =~ ^[Yy]$ ]]; then echo "Exiting"; exit 1; fi
-echo "Pushing develop."
-git push
+echo "Pushing ${branchPrefix}develop."
+# git push
