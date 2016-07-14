84d6ca4bb42f46b462808b4735cfe94e59f845b1
Mark Czotter <mczotter@b2international.com>
2016-07-14 10:42:31 +0200

    Add back (deprecated) 4.x Store API for merge review store

---
+++ core/com.b2international.snowowl.datastore.comp46/src/com/b2international/snowowl/datastore/store/query/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @deprecated As of release 5.0 nested index implementation is no longer supported, if possible switch to new index API
+ */
+package com.b2international.snowowl.datastore.store.query;
