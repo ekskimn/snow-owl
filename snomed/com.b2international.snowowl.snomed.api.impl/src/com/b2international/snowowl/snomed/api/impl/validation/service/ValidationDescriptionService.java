2a16fa4e585d0141d20b49f111096b6677c060d8
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-08-25 10:03:40 -0700

    WRP-2834: Added isTermUniqueWithinHierarchy function to ValidationDescriptionService

--- snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/validation/service/ValidationDescriptionService.java
+++ snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/validation/service/ValidationDescriptionService.java
@@ -6,15 +6,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.ihtsdo.drools.domain.Description;
-
 import com.b2international.commons.http.ExtendedLocale;
-import com.b2international.snowowl.core.api.IBranchPath;
 import com.b2international.snowowl.eventbus.IEventBus;
+import com.b2international.snowowl.snomed.Concept;
+import com.b2international.snowowl.snomed.Description;
 import com.b2international.snowowl.snomed.SnomedConstants.LanguageCodeReferenceSetIdentifierMapping;
+import com.b2international.snowowl.snomed.api.domain.browser.ISnomedBrowserConcept;
 import com.b2international.snowowl.snomed.api.impl.DescriptionService;
+import com.b2international.snowowl.snomed.api.impl.validation.domain.ValidationConcept;
 import com.b2international.snowowl.snomed.api.impl.validation.domain.ValidationSnomedDescription;
+import com.b2international.snowowl.snomed.core.domain.ISnomedConcept;
 import com.b2international.snowowl.snomed.core.domain.ISnomedDescription;
+import com.b2international.snowowl.snomed.core.domain.SnomedConcept;
+import com.b2international.snowowl.snomed.core.domain.SnomedConcepts;
 import com.b2international.snowowl.snomed.core.domain.SnomedDescriptions;
 import com.b2international.snowowl.snomed.datastore.server.request.SnomedRequests;
 
@@ -57,8 +61,10 @@ public class ValidationDescriptionService implements org.ihtsdo.drools.service.D
 		for (ISnomedDescription iSnomedDescription : descriptions) {
 			if (iSnomedDescription.getTerm().equals(exactTerm)) {
 				matches.add(new ValidationSnomedDescription(iSnomedDescription, iSnomedDescription.getConceptId()));
+				iSnomedDescription.getConceptId()
 			}
 		}
+
 		return matches;
 	}
 	
@@ -70,13 +76,55 @@ public class ValidationDescriptionService implements org.ihtsdo.drools.service.D
 				.build(branchPath)
 				.executeSync(bus);
 
-		Set<Description> matches = new HashSet<>();
+		Set<Concept> matches = new HashSet<>();
 		for (ISnomedDescription iSnomedDescription : descriptions) {
 			if (iSnomedDescription.getTerm().equals(exactTerm)) {
 				matches.add(new ValidationSnomedDescription(iSnomedDescription, iSnomedDescription.getConceptId()));
 			}
 		}
 		return matches;
+	}
+
+
+	@Override
+	public boolean isTermUniqueWithinHierarchy(String exactTerm, String semanticTag, boolean isActive) {
+		Set<Concept> matches = new HashSet<>();
+		// TODO This doesn't work due to lack of constructor for ValidationConcept(
+		 // 1 - findActiveDescriptionsByExactTerm -- or use descriptionService
+		//  2 - retrieve concepts
+		//  3 - return true/false based on fsn tag
+		
+		final SnomedDescriptions descriptions = SnomedRequests.prepareSearchDescription()
+				.filterByActive(isActive)
+				.filterByTerm(exactTerm)
+				.build(branchPath)
+				.executeSync(bus);
+
+		Set<String> conceptIds = new HashSet<>();
+		
+		for (ISnomedDescription iSnomedDescription : descriptions) {
+			if (iSnomedDescription.getTerm().equals(exactTerm)) {
+				conceptIds.add(iSnomedDescription.getConceptId());
+			}
+		}
+		
+		SnomedConcepts concepts = SnomedRequests
+			     .prepareSearchConcept()
+			     .setComponentIds(conceptIds)
+			     .setExpand("fsn()")
+			     .build(branchPath)
+			     .executeSync(bus);
+		
+		for (ISnomedConcept concept : concepts) {
+			if (concept.getFsn().getTerm().endsWith(semanticTag)) {
+				return false;
+			}
+		}
+		
+		
+		return true;
+		
+		
 	}
 
 }
