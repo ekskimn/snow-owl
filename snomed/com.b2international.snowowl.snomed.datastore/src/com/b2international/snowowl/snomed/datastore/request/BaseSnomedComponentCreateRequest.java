b48441e78bdbcb5b2913c437bc6a8a795c787263
András Péteri <apeteri@b2international.com>
2016-08-30 10:03:36 +0200

    [snomed] Fix template string in BaseSnomedComponentCreateRequest output

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/BaseSnomedComponentCreateRequest.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/BaseSnomedComponentCreateRequest.java
@@ -102,7 +102,7 @@ public abstract class BaseSnomedComponentCreateRequest extends BaseRequest<Trans
 						return;
 					}
 					
-					LOGGER.warn("SCTID generation returned component id already in use: %s.  Requesting another...", componentId);
+					LOGGER.warn("SCTID generation returned component id already in use: {}.  Requesting another...", componentId);
 				}
 				
 				throw new BadRequestException("Couldn't generate unique identifier for %s after %d attempts.", type, ID_GENERATION_ATTEMPTS); 
