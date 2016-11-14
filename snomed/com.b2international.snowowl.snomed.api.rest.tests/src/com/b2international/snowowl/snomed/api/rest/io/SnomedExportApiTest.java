238367c0cae919a2afd1272319a55d831995cbc6
Gabor Nagy <gnagy@b2international.com>
2016-11-14 17:23:52 +0100

    WRP-3238: Add more test case for delta export
    - test delta export with start and end dates from version branch
    - test delta export with start and end dates and include unpublished
    flag
    
    https://jira.ihtsdotools.org/browse/WRP-3238

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/io/SnomedExportApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/io/SnomedExportApiTest.java
@@ -18,6 +18,7 @@ package com.b2international.snowowl.snomed.api.rest.io;
 import static com.b2international.snowowl.datastore.BranchPathUtils.createMainPath;
 import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.MODULE_SCT_CORE;
 import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.ROOT_CONCEPT;
+import static com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants.ACCEPTABLE_ACCEPTABILITY_MAP;
 import static com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants.PREFERRED_ACCEPTABILITY_MAP;
 import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.whenCreatingVersion;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCreated;
@@ -37,11 +38,16 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.b2international.commons.Pair;
+import com.b2international.snowowl.core.branch.Branch;
 import com.b2international.snowowl.core.date.DateFormats;
 import com.b2international.snowowl.core.date.Dates;
+import com.b2international.snowowl.datastore.BranchPathUtils;
 import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
 import com.b2international.snowowl.snomed.api.rest.SnomedComponentType;
+import com.b2international.snowowl.snomed.core.domain.CaseSignificance;
 import com.b2international.snowowl.snomed.core.domain.CharacteristicType;
+import com.b2international.snowowl.snomed.core.domain.DefinitionStatus;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableList;
@@ -79,37 +85,13 @@ public class SnomedExportApiTest extends AbstractSnomedExportApiTest {
 	}
 	
 	@Test
-	public void exportUnpublishedDeltaRelationships() {
+	public void exportUnpublishedDeltaRelationships() throws Exception {
 		
-		final Map<?, ?> conceptRequestBody = givenConceptRequestBody(null, ROOT_CONCEPT, MODULE_SCT_CORE, PREFERRED_ACCEPTABILITY_MAP, false);
-		assertComponentCreated(createMainPath(), SnomedComponentType.CONCEPT, conceptRequestBody);
+		assertNewVersionCreated();
 		
-		final String dateForNewVersion = getDateForNewVersion("SNOMEDCT");
-		
-		whenCreatingVersion(dateForNewVersion, dateForNewVersion)
-			.then().assertThat().statusCode(201);
-		
-		givenAuthenticatedRequest(ADMIN_API)
-			.when().get("/codesystems/SNOMEDCT/versions/{id}", dateForNewVersion)
-			.then().assertThat().statusCode(200);
-		
-		final Map<?, ?> statedRequestBody = givenRelationshipRequestBody(DISEASE, TEMPORAL_CONTEXT, FINDING_CONTEXT, MODULE_SCT_CORE,
-				"New relationship on MAIN");
-		final String statedRelationshipId = assertComponentCreated(createMainPath(), SnomedComponentType.RELATIONSHIP, statedRequestBody);
-		assertComponentHasProperty(createMainPath(), SnomedComponentType.RELATIONSHIP, statedRelationshipId, "characteristicType",
-				CharacteristicType.STATED_RELATIONSHIP.name());
-
-		final Map<?, ?> inferredRequestBody = givenRelationshipRequestBody(DISEASE, TEMPORAL_CONTEXT, FINDING_CONTEXT, MODULE_SCT_CORE,
-				CharacteristicType.INFERRED_RELATIONSHIP, "New relationship on MAIN");
-		final String inferredRelationshipId = assertComponentCreated(createMainPath(), SnomedComponentType.RELATIONSHIP, inferredRequestBody);
-		assertComponentHasProperty(createMainPath(), SnomedComponentType.RELATIONSHIP, inferredRelationshipId, "characteristicType",
-				CharacteristicType.INFERRED_RELATIONSHIP.name());
-
-		final Map<?, ?> additionalRequestBody = givenRelationshipRequestBody(BLEEDING, TEMPORAL_CONTEXT, FINDING_CONTEXT, MODULE_SCT_CORE,
-				CharacteristicType.ADDITIONAL_RELATIONSHIP, "New relationship on MAIN");
-		final String additionalRelationshipId = assertComponentCreated(createMainPath(), SnomedComponentType.RELATIONSHIP, additionalRequestBody);
-		assertComponentHasProperty(createMainPath(), SnomedComponentType.RELATIONSHIP, additionalRelationshipId, "characteristicType",
-				CharacteristicType.ADDITIONAL_RELATIONSHIP.name());
+		final String statedRelationshipId = createStatedRelationshipOnMain();
+		final String inferredRelationshipId = createInferredRelationshipOnMain();
+		final String additionalRelationshipId = createAdditionalRelationshipOnMain();
 
 		final String transientEffectiveTime = "20170131";
 		
@@ -128,54 +110,243 @@ public class SnomedExportApiTest extends AbstractSnomedExportApiTest {
 		
 		final File exportArchive = assertExportFileCreated(exportId);
 		
-		final List<String> statedLineElements = ImmutableList.<String> of(statedRelationshipId, transientEffectiveTime, "1", MODULE_SCT_CORE, DISEASE,
+		String statedLine = getComponentLine(ImmutableList.<String> of(statedRelationshipId, transientEffectiveTime, "1", MODULE_SCT_CORE, DISEASE,
 				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.STATED_RELATIONSHIP.getConceptId(),
-				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER);
-		final String statedLine = Joiner.on("\t").join(statedLineElements);
-
-		final List<String> inferredLineElements = ImmutableList.<String> of(inferredRelationshipId, transientEffectiveTime, "1", MODULE_SCT_CORE, DISEASE,
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		String inferredLine = getComponentLine(ImmutableList.<String> of(inferredRelationshipId, transientEffectiveTime, "1", MODULE_SCT_CORE, DISEASE,
 				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.INFERRED_RELATIONSHIP.getConceptId(),
-				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER);
-		final String inferredLine = Joiner.on("\t").join(inferredLineElements);
-		
-		final List<String> additionalLineElements = ImmutableList.<String> of(additionalRelationshipId, transientEffectiveTime, "1", MODULE_SCT_CORE, BLEEDING,
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		String additionalLine = getComponentLine(ImmutableList.<String> of(additionalRelationshipId, transientEffectiveTime, "1", MODULE_SCT_CORE, BLEEDING,
 				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.ADDITIONAL_RELATIONSHIP.getConceptId(),
-				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER);
-		final String additionalLine = Joiner.on("\t").join(additionalLineElements);
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
 		
-		final Multimap<String, String> fileToLinesMap = ArrayListMultimap.<String, String>create();
+		final Multimap<String, Pair<Boolean, String>> fileToLinesMap = ArrayListMultimap.<String, Pair<Boolean, String>>create();
 		
-		fileToLinesMap.put("sct2_StatedRelationship", statedLine);
-		fileToLinesMap.put("sct2_Relationship", inferredLine);
-		fileToLinesMap.put("sct2_Relationship", additionalLine);
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(true, statedLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(true, inferredLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(true, additionalLine));
+		
+		assertArchiveContainsLines(exportArchive, fileToLinesMap);
+	}
+
+	@Test
+	public void exportDeltaInDateRangeFromVersionBranch() throws Exception {
+		
+		assertNewVersionCreated();
+		
+		final String statedRelationshipId = createStatedRelationshipOnMain();
+		final String inferredRelationshipId = createInferredRelationshipOnMain();
+		final String additionalRelationshipId = createAdditionalRelationshipOnMain();
+		
+		Date newVersionDate = assertNewVersionCreated();
+		
+		String versionName = Dates.formatByGmt(newVersionDate);
+		String versionEffectiveDate = Dates.formatByGmt(newVersionDate, DateFormats.SHORT);
+		String versionPath = BranchPathUtils.createVersionPath(versionName).getPath();
+		
+		final Map<Object, Object> config = ImmutableMap.builder()
+				.put("type", "DELTA")
+				.put("branchPath", versionPath)
+				.put("deltaStartEffectiveTime", versionEffectiveDate)
+				.put("deltaEndEffectiveTime", versionEffectiveDate)
+				.build();
+			
+		final String exportId = assertExportConfigurationCanBeCreated(config);
+		
+		assertExportConfiguration(exportId)
+			.and().body("type", equalTo("DELTA"))
+			.and().body("branchPath", equalTo(versionPath))
+			.and().body("deltaStartEffectiveTime", equalTo(versionEffectiveDate))
+			.and().body("deltaEndEffectiveTime", equalTo(versionEffectiveDate));
+		
+		final File exportArchive = assertExportFileCreated(exportId);
+		
+		String statedLine = getComponentLine(ImmutableList.<String> of(statedRelationshipId, versionEffectiveDate, "1", MODULE_SCT_CORE, DISEASE,
+				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.STATED_RELATIONSHIP.getConceptId(),
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		String inferredLine = getComponentLine(ImmutableList.<String> of(inferredRelationshipId, versionEffectiveDate, "1", MODULE_SCT_CORE, DISEASE,
+				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.INFERRED_RELATIONSHIP.getConceptId(),
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		String additionalLine = getComponentLine(ImmutableList.<String> of(additionalRelationshipId, versionEffectiveDate, "1", MODULE_SCT_CORE, BLEEDING,
+				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.ADDITIONAL_RELATIONSHIP.getConceptId(),
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		
+		final Multimap<String, Pair<Boolean, String>> fileToLinesMap = ArrayListMultimap.<String, Pair<Boolean, String>>create();
+		
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(true, statedLine));
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(false, inferredLine));
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(false, additionalLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(true, inferredLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(true, additionalLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(false, statedLine));
 		
 		assertArchiveContainsLines(exportArchive, fileToLinesMap);
 	}
 	
-	private Collection<String> getEffectiveDates(final String uniqueId) {
+	@Test
+	public void exportDeltaInDateRangeAndUnpublishedComponents() throws Exception {
+		
+		// create new version
+		assertNewVersionCreated();
+		
+		final String statedRelationshipId = createStatedRelationshipOnMain();
+		final String inferredRelationshipId = createInferredRelationshipOnMain();
+		final String additionalRelationshipId = createAdditionalRelationshipOnMain();
+		
+		// version new relationships
+		Date relationshipVersionDate = assertNewVersionCreated();
+		
+		// create new concept
+		final Map<?, ?> conceptRequestBody = givenConceptRequestBody(null, ROOT_CONCEPT, MODULE_SCT_CORE, PREFERRED_ACCEPTABILITY_MAP, false);
+		String conceptId = assertComponentCreated(createMainPath(), SnomedComponentType.CONCEPT, conceptRequestBody);
+		
+		// version new concept
+		Date conceptVersionDate = assertNewVersionCreated();
+		String conceptEffectiveDate = Dates.formatByGmt(conceptVersionDate, DateFormats.SHORT);
+
+		// create new description
+		String descriptionTerm = "Exported Description";
+		Map<?, ?> descriptionRequestBody = ImmutableMap.builder()
+			.put("conceptId", conceptId)
+			.put("moduleId", MODULE_SCT_CORE)
+			.put("typeId", Concepts.SYNONYM)
+			.put("term", descriptionTerm)
+			.put("languageCode", "en")
+			.put("acceptability", ACCEPTABLE_ACCEPTABILITY_MAP)
+			.put("commitComment", "new description")
+			.build();
+		
+		String descriptionId = assertComponentCreated(createMainPath(), SnomedComponentType.DESCRIPTION, descriptionRequestBody);
+		
+		// do not version description
+		
+		String versionEffectiveDate = Dates.formatByGmt(relationshipVersionDate, DateFormats.SHORT);
+		
+		final Map<Object, Object> config = ImmutableMap.builder()
+				.put("type", "DELTA")
+				.put("branchPath", Branch.MAIN_PATH)
+				.put("deltaStartEffectiveTime", versionEffectiveDate)
+				.put("deltaEndEffectiveTime", versionEffectiveDate)
+				.put("includeUnpublished", true)
+				.build();
+			
+		final String exportId = assertExportConfigurationCanBeCreated(config);
+		
+		assertExportConfiguration(exportId)
+			.and().body("type", equalTo("DELTA"))
+			.and().body("branchPath", equalTo(Branch.MAIN_PATH))
+			.and().body("deltaStartEffectiveTime", equalTo(versionEffectiveDate))
+			.and().body("deltaEndEffectiveTime", equalTo(versionEffectiveDate))
+			.and().body("includeUnpublished", equalTo(true));
+		
+		final File exportArchive = assertExportFileCreated(exportId);
+		
+		String statedLine = getComponentLine(ImmutableList.<String> of(statedRelationshipId, versionEffectiveDate, "1", MODULE_SCT_CORE, DISEASE,
+				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.STATED_RELATIONSHIP.getConceptId(),
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		String inferredLine = getComponentLine(ImmutableList.<String> of(inferredRelationshipId, versionEffectiveDate, "1", MODULE_SCT_CORE, DISEASE,
+				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.INFERRED_RELATIONSHIP.getConceptId(),
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		String additionalLine = getComponentLine(ImmutableList.<String> of(additionalRelationshipId, versionEffectiveDate, "1", MODULE_SCT_CORE, BLEEDING,
+				FINDING_CONTEXT, "0", TEMPORAL_CONTEXT, CharacteristicType.ADDITIONAL_RELATIONSHIP.getConceptId(),
+				Concepts.EXISTENTIAL_RESTRICTION_MODIFIER));
+		
+		String conceptLine = getComponentLine(ImmutableList.<String> of(conceptId, conceptEffectiveDate, "1", MODULE_SCT_CORE, 
+				DefinitionStatus.PRIMITIVE.getConceptId()));
+
+		String descriptionLine = getComponentLine(ImmutableList.<String> of(descriptionId, "", "1", MODULE_SCT_CORE, conceptId, "en",
+				Concepts.SYNONYM, descriptionTerm, CaseSignificance.INITIAL_CHARACTER_CASE_INSENSITIVE.getConceptId()));
+
+		final Multimap<String, Pair<Boolean, String>> fileToLinesMap = ArrayListMultimap.<String, Pair<Boolean, String>>create();
+		
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(true, statedLine));
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(false, inferredLine));
+		fileToLinesMap.put("sct2_StatedRelationship", Pair.of(false, additionalLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(true, inferredLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(true, additionalLine));
+		fileToLinesMap.put("sct2_Relationship", Pair.of(false, statedLine));
+		
+		fileToLinesMap.put("sct2_Concept", Pair.of(false, conceptLine));
+		fileToLinesMap.put("sct2_Description", Pair.of(true, descriptionLine));
+		
+		assertArchiveContainsLines(exportArchive, fileToLinesMap);
+	}
+
+	private String createAdditionalRelationshipOnMain() {
+		final Map<?, ?> additionalRequestBody = givenRelationshipRequestBody(BLEEDING, TEMPORAL_CONTEXT, FINDING_CONTEXT, MODULE_SCT_CORE,
+				CharacteristicType.ADDITIONAL_RELATIONSHIP, "New relationship on MAIN");
+		final String additionalRelationshipId = assertComponentCreated(createMainPath(), SnomedComponentType.RELATIONSHIP, additionalRequestBody);
+		assertComponentHasProperty(createMainPath(), SnomedComponentType.RELATIONSHIP, additionalRelationshipId, "characteristicType",
+				CharacteristicType.ADDITIONAL_RELATIONSHIP.name());
+		return additionalRelationshipId;
+	}
+
+	private String createInferredRelationshipOnMain() {
+		final Map<?, ?> inferredRequestBody = givenRelationshipRequestBody(DISEASE, TEMPORAL_CONTEXT, FINDING_CONTEXT, MODULE_SCT_CORE,
+				CharacteristicType.INFERRED_RELATIONSHIP, "New relationship on MAIN");
+		final String inferredRelationshipId = assertComponentCreated(createMainPath(), SnomedComponentType.RELATIONSHIP, inferredRequestBody);
+		assertComponentHasProperty(createMainPath(), SnomedComponentType.RELATIONSHIP, inferredRelationshipId, "characteristicType",
+				CharacteristicType.INFERRED_RELATIONSHIP.name());
+		return inferredRelationshipId;
+	}
+
+	private String createStatedRelationshipOnMain() {
+		final Map<?, ?> statedRequestBody = givenRelationshipRequestBody(DISEASE, TEMPORAL_CONTEXT, FINDING_CONTEXT, MODULE_SCT_CORE,
+				"New relationship on MAIN");
+		final String statedRelationshipId = assertComponentCreated(createMainPath(), SnomedComponentType.RELATIONSHIP, statedRequestBody);
+		assertComponentHasProperty(createMainPath(), SnomedComponentType.RELATIONSHIP, statedRelationshipId, "characteristicType",
+				CharacteristicType.STATED_RELATIONSHIP.name());
+		return statedRelationshipId;
+	}
+
+	private String getComponentLine(List<String> lineElements) {
+		return Joiner.on("\t").join(lineElements);
+	}
+	
+	private Date assertNewVersionCreated() {
+		final Map<?, ?> conceptRequestBody = givenConceptRequestBody(null, ROOT_CONCEPT, MODULE_SCT_CORE, PREFERRED_ACCEPTABILITY_MAP, false);
+		assertComponentCreated(createMainPath(), SnomedComponentType.CONCEPT, conceptRequestBody);
+		
+		final Date dateForNewVersion = getDateForNewVersion("SNOMEDCT");
+		
+		String versionName = Dates.formatByGmt(dateForNewVersion);
+		String versionEffectiveDate = Dates.formatByGmt(dateForNewVersion, DateFormats.SHORT);
+		
+		whenCreatingVersion(versionName, versionEffectiveDate)
+			.then().assertThat().statusCode(201);
+		
+		givenAuthenticatedRequest(ADMIN_API)
+			.when().get("/codesystems/SNOMEDCT/versions/{id}", versionName)
+			.then().assertThat().statusCode(200);
+		
+		return dateForNewVersion;
+	}
+
+	private Collection<String> getEffectiveDates(final String terminologyShortName) {
+		
 		final Map<?, ?> response = givenAuthenticatedRequest(ADMIN_API)
 			.and().contentType(ContentType.JSON)
-			.when().get("/codesystems/{shortName}/versions", uniqueId)
+			.when().get("/codesystems/{shortName}/versions", terminologyShortName)
 			.then().extract().body().as(Map.class);
 		
 		if (!response.containsKey("items")) {
 			return Collections.emptyList();
-		} else {
-			final List<String> effectiveDates = Lists.newArrayList();
-			@SuppressWarnings("unchecked")
-			final List<Map<?, ?>> items = (List<Map<?, ?>>) response.get("items");
-			for (final Map<?, ?> version : items) {
-				final String effectiveDate = (String) version.get("effectiveDate");
-				effectiveDates.add(effectiveDate);
-			}
-			
-			return effectiveDates;
 		}
+		
+		final List<String> effectiveDates = Lists.newArrayList();
+		@SuppressWarnings("unchecked")
+		final List<Map<?, ?>> items = (List<Map<?, ?>>) response.get("items");
+		for (final Map<?, ?> version : items) {
+			final String effectiveDate = (String) version.get("effectiveDate");
+			effectiveDates.add(effectiveDate);
+		}
+		
+		return effectiveDates;
 	}
 	
-	private String getDateForNewVersion(final String uniqueId) {
+	private Date getDateForNewVersion(final String terminologyShortname) {
 		Date latestEffectiveDate = new Date();
-		for (final String effectiveDate : getEffectiveDates(uniqueId)) {
+		for (final String effectiveDate : getEffectiveDates(terminologyShortname)) {
 			Date effDate = Dates.parse(effectiveDate, DateFormats.SHORT);
 			if (latestEffectiveDate.before(effDate)) {
 				latestEffectiveDate = effDate;
@@ -186,7 +357,7 @@ public class SnomedExportApiTest extends AbstractSnomedExportApiTest {
 		calendar.setTime(latestEffectiveDate);
 		calendar.add(Calendar.DATE, 1);
 
-		return Dates.formatByGmt(calendar.getTime(), DateFormats.SHORT);
+		return calendar.getTime();
 	}
 	
 }
