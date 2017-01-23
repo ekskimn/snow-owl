5b6c1855bb6f29be120d223d78c29ba1cdd99ed3
Kai Kewley <kaikewley@gmail.com>
2017-01-23 14:31:50 +0000

    APDS-18 New description inactivation reasons.

--- snomed/com.b2international.snowowl.snomed.model/src/com/b2international/snowowl/snomed/SnomedConstants.java
+++ snomed/com.b2international.snowowl.snomed.model/src/com/b2international/snowowl/snomed/SnomedConstants.java
@@ -260,6 +260,7 @@ public abstract class SnomedConstants {
 		public static final String PENDING_MOVE = "900000000000492006";
 		public static final String CONCEPT_NON_CURRENT = "900000000000495008";
 		public static final String NONCONFORMANCE_TO_EDITORIAL_POLICY = "723277005";
+		public static final String NOT_SEMANTICALLY_EQUIVALENT = "723278000";
 		
 		// Historical reference sets
 		public static final String REFSET_ALTERNATIVE_ASSOCIATION = "900000000000530003";
