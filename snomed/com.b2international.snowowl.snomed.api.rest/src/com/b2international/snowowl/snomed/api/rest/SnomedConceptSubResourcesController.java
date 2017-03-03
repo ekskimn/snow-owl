83c38b0f6e9dc11b9dff86a6efe31c455fc8112d
Gabor Nagy <gnagy@b2international.com>
2017-03-03 17:24:32 +0100

    TCC-5: Re-enable SnomedConceptSubResourcesController
    https://jira.ihtsdotools.org/browse/TCC-5

--- snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/SnomedConceptSubResourcesController.java
+++ snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/SnomedConceptSubResourcesController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import com.b2international.commons.http.AcceptHeader;
@@ -58,10 +59,12 @@ import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
 
 /**
+ * XXX This whole class can be replaced by using the appropriate expand parameters since Snow Owl 5.6
+ * 
  * @since 1.0
  */
 @Api("Concepts")
-//@RestController
+@RestController
 @RequestMapping(
 		produces={ AbstractRestService.SO_MEDIA_TYPE })
 public class SnomedConceptSubResourcesController extends AbstractSnomedRestService {
