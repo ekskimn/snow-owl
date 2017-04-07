39a962a920dec2b34547d20fcc0ec5aef2429e80
Endre Kovacs <ekovacs@b2international.com>
2017-04-07 14:17:06 +0200

    allowing ECL evaluation to time out / unblock after 5 mins.

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/SnomedConceptSearchRequest.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/SnomedConceptSearchRequest.java
@@ -28,6 +28,7 @@ import static com.google.common.collect.Maps.newHashMap;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import com.b2international.collections.longs.LongCollection;
 import com.b2international.commons.collect.LongSets;
@@ -208,7 +209,12 @@ final class SnomedConceptSearchRequest extends SnomedSearchRequest<SnomedConcept
 		
 		if (containsKey(OptionKey.ECL)) {
 			final String ecl = getString(OptionKey.ECL);
-			queryBuilder.must(SnomedRequests.prepareEclEvaluation(ecl).build().execute(context).getSync());
+			queryBuilder.must(SnomedRequests.prepareEclEvaluation(ecl)
+					.build()
+					.execute(context)
+					//XXX: ECL evaluation may fire sub requests that may be aimed at an already FULL event bus
+					// thus the need for allowing this request to time out to avoid deadlock. 
+					.getSync(5, TimeUnit.MINUTES)); 
 		}
 		
 		Expression searchProfileQuery = null;
