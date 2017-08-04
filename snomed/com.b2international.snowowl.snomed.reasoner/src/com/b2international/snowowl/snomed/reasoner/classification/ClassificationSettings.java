717ccaecd7df7c484e1c6dac98dbd2cc98df4360
Gabor Nagy <gnagy@b2international.com>
2017-08-04 15:18:38 +0200

    CLASS-4: Add useExternalService option to classify request
    https://jira.ihtsdotools.org/browse/CLASS-4

--- snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/classification/ClassificationSettings.java
+++ snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/classification/ClassificationSettings.java
@@ -25,7 +25,7 @@ import java.util.UUID;
 import com.b2international.snowowl.core.api.IBranchPath;
 import com.b2international.snowowl.datastore.oplock.impl.DatastoreLockContextDescriptions;
 import com.b2international.snowowl.snomed.reasoner.model.ConceptDefinition;
-import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
 
 /**
  * Carries all parameters required for starting a classification for a branch.
@@ -41,6 +41,7 @@ public class ClassificationSettings implements Serializable {
 	
 	private String parentContextDescription = DatastoreLockContextDescriptions.CLASSIFY_WITH_REVIEW;
 	private String reasonerId;
+	private boolean useExternalService;
 	
 	public ClassificationSettings(String userId, IBranchPath snomedBranchPath) {
 		checkNotNull(userId, "User identifier may not be null.");
@@ -48,6 +49,7 @@ public class ClassificationSettings implements Serializable {
 		
 		this.userId = userId;
 		this.snomedBranchPath = snomedBranchPath;
+		this.useExternalService = false;
 	}
 	
 	public ClassificationSettings withAdditionalDefinitions(List<ConceptDefinition> additionalDefinitions) {
@@ -67,6 +69,11 @@ public class ClassificationSettings implements Serializable {
 
 	public ClassificationSettings withReasonerId(String reasonerId) {
 		this.reasonerId = reasonerId;
+		return this;
+	}
+	
+	public ClassificationSettings withExternalService(boolean useExternalService) {
+		this.useExternalService = useExternalService;
 		return this;
 	}
 
@@ -94,15 +101,20 @@ public class ClassificationSettings implements Serializable {
 		return reasonerId;
 	}
 	
+	public boolean isUseExternalService() {
+		return useExternalService;
+	}
+	
 	@Override
 	public String toString() {
-		return Objects.toStringHelper(this)
+		return MoreObjects.toStringHelper(this)
 				.add("classificationId", classificationId)
 				.add("userId", userId)
 				.add("snomedBranchPath", snomedBranchPath)
 				.add("additionalDefinitions", additionalDefinitions)
 				.add("parentContextDescription", parentContextDescription)
 				.add("reasonerId", reasonerId)
+				.add("useExternalService", useExternalService)
 				.toString();
 	}
 }
